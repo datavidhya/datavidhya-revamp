e32aaa30fe958a65480206c75b50af3d8e049d59
Raj Kishan Verma <rajkishanvrma@gmail.com>
2025-05-09 23:10:51 +0530

    fix:repo

--- src/components/revamp/hero/HeroSection.tsx
+++ src/components/revamp/hero/HeroSection.tsx
@@ -90,7 +90,7 @@ export default function HeroSection() {
           <img
             src="/revamp/realBg1.svg"
             className="absolute -bottom:[35%] xl:-bottom-[15%] -left-1/4 xl:-left-1/4 h-[740px] w-full -rotate-12"
-            alt="Bg decoration 1"
+            alt="Bg decoration img 1"
           />
         </motion.div>
         <motion.div
