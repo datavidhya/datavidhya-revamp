4638bb1560ef881f660594b502e172c6ce1dd8a2
Raj Kishan Verma <rajkishanvrma@gmail.com>
2025-06-08 16:27:41 +0530

    fix:combo pack images

--- src/app/(home)/combopack/page.tsx
+++ src/app/(home)/combopack/page.tsx
@@ -11,15 +11,27 @@ const combopack = () => {
   return (
     <div>
       <NavigationBar />
-      <img src="/combopack/heroImage.png" className="mx-auto mt-12" alt="" />
+      <img
+        src="/combopack/heroImage.png"
+        className="hidden md:block mx-auto mt-12"
+        alt=""
+      />
+      <img
+        src="/combopack/heroImageMobile.png"
+        className="block md:hidden mx-auto mt-6"
+        alt=""
+      />
       {/* <CombeHeroSection /> */}
       <img
         src={"/combopack/techStack.png"}
-        className="hidden md:block mx-auto scale-[1.25] md:scale-100 my-36"
+        className="hidden md:block mx-auto my-36"
+      ></img>{" "}
+      <img
+        src={"/combopack/techStackMobile.png"}
+        className="block md:hidden  mx-auto my-6"
       ></img>
       <DataStackSection />
       <ComboProjectReview />
-
       <CourseDetailSection />
       <FAQsSection />
       <Footer />
