000b3a3731915a746904d1a6fa1ac5729fd03f7b
Raj Kishan Verma <rajkishanvrma@gmail.com>
2025-06-09 19:30:00 +0530

    checking deployment

--- src/components/revamp/combopackSection/CombeHeroSection.tsx
+++ src/components/revamp/combopackSection/CombeHeroSection.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { HiCheckCircle } from "react-icons/hi";
+
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 interface prop {
   text: string;
-  num: number;
+  num?: number;
 }
 export const WhiteBox = ({ text, num }: prop) => {
   return (
-    <div className="flex flex-col bg-[#f4f0fc] rounded-[6px]">
+    <div className="w-[100px] h-[60px] flex flex-col items-center justify-center bg-[#f4f0fc] rounded-[6px]">
       <p
         className={`font-semibold text-[20px] leading-[140%] text-[#5a31c3] ${inter.className}`}
       >
@@ -19,11 +21,19 @@ export const WhiteBox = ({ text, num }: prop) => {
     </div>
   );
 };
+export const TickText = ({ text }: prop) => {
+  return (
+    <div className="flex items-center">
+      <HiCheckCircle className="text-white size-[20px]" />
+      <p className="text-white text-xl leading-[24px] font-semibold">{text}</p>
+    </div>
+  );
+};
 
 const CombeHeroSection = () => {
   return (
-    <div className="w-full flex bg-gradient-to-r from-[#7154f2] to-[#9b49ed] p-10">
-      <div className="flex flex-col items-center bg-white p-5 border ">
+    <div className="w-full max-w-[1260px] mx-auto border border-[#d7d7d7d] rounded-2xl flex bg-gradient-to-r from-[#7154f2] to-[#9b49ed] p-10">
+      <div className="flex flex-col items-center bg-white p-5 border rounded-2xl">
         <img src="" alt="" />
         <h1>Zero to Hero</h1>
         <h1>Data Engineering</h1>
@@ -32,14 +42,24 @@ const CombeHeroSection = () => {
           Buy Now
         </button>
       </div>
-      <div>
-        <div></div>
-        <div className="flex">
-          <WhiteBox num={12} text="" />
-          <WhiteBox num={12} text="" />
-          <WhiteBox num={12} text="" />
-          <WhiteBox num={12} text="" />
-          <WhiteBox num={12} text="" />
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-8 flex-wrap">
+          <TickText text="afasdca" />
+          <TickText text="afasdca" />
+          <TickText text="afasdca" />
+          <TickText text="afasdca" />
+
+          <TickText text="afasdca" />
+          <TickText text="afasdca" />
+          <TickText text="afasdca" />
+          <TickText text="afasdca" />
+        </div>
+        <div className="flex gap-8">
+          <WhiteBox num={12} text="adfgba" />
+          <WhiteBox num={12} text="adfgba" />
+          <WhiteBox num={12} text="adfgba" />
+          <WhiteBox num={12} text="adfgba" />
+          <WhiteBox num={12} text="adfgba" />
         </div>
       </div>
     </div>
