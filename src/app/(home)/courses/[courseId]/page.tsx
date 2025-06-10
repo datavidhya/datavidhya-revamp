b7c9b5eb12c421804bec4508c99784c56dc2ec06
yashdev9274 <yashdev.yvd@gmail.com>
2025-06-10 17:20:41 +0530

    feat: implement CourseIdPage for admin and update UI component: Add admin CourseIdPage and reusable IconBadge component. Implements a new admin-specific CourseIdPage with functionality to handle course slugs and redirects for invalid courses. Adds a reusable IconBadge UI component with customizable variants for styling. Refactors the existing CourseIdPage to improve maintainability and align with the admin page structure.

--- src/app/(home)/courses/[courseId]/page.tsx
+++ src/app/(home)/courses/[courseId]/page.tsx
@@ -1,20 +1,58 @@
-import { NextPage } from 'next';
+import {  PrismaClient } from '@prisma/client';
+import { redirect } from 'next/navigation';
+// import { NextPage } from 'next';
+
+// import { Prisma } from "@prisma/client";
 
 
-interface CourseIdPageProps {
-    params: {
-        courseId: string;
-    };
-}
+// interface CourseIdPageProps {
+//     params: {
+//         courseId: string;
+//     };
+// }
 
-const CourseIdPage: NextPage<CourseIdPageProps> = async ({ params }) => {
-    const { courseId } = await params; 
+const prisma = new PrismaClient();
 
-    return (
+// const CourseIdPage: NextPage<CourseIdPageProps> = async ({ params }) => {
+//     const { courseId } =  params;
+
+//     console.log('CourseId:', courseId)
+//     const course = await prisma.course.findUnique({
+//         where: {
+//             id: Number(courseId),
+//         },
+//     });
+
+//     if (!course) {
+//         return <div>Course not found</div>; // Handle case where course is not found
+//     }
+
+//     return (
+//         <div>
+//             <h1>Course ID: {courseId}</h1>
+//         </div>
+//     );
+// };
+
+const CourseIdPage= async({params}:{params: {courseId: string}})=>{
+
+    const {courseId} = await params
+    const course = await prisma.course.findUnique({
+        where:{
+            slug: courseId
+        }
+    })
+
+    if(!course){
+        return redirect('/');
+    }
+    return(
         <div>
-            <h1>Course ID: {courseId}</h1>
+            CourseId: {courseId}
         </div>
-    );
-};
+    )
+} 
+
 
 export default CourseIdPage;
+
