b689a037bb6f8457f649a8801d5d5e065d7ff275
Raj Kishan Verma <rajkishanvrma@gmail.com>
2025-06-17 09:10:54 +0530

    fix:what you get

--- src/components/revamp/CourseOverview/WhatWillYouLearn.tsx
+++ src/components/revamp/CourseOverview/WhatWillYouLearn.tsx
@@ -18,15 +18,17 @@ const WhatWillYouLearn = () => {
   return (
     <div>
       <p className={`text-5xl mt-12 mb-8 font-bold ${inter.className}`}>
-        What Will You Learn ?
+        What Will You Get ?
       </p>
       <div className="grid grid-cols-2 gap-6 mt-3">
-        <Items text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum numquam cumque" />
-        <Items text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum numquam cumque" />
-        <Items text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum numquam cumque" />
-        <Items text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum numquam cumque" />
-        <Items text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum numquam cumque" />
-        <Items text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum numquam cumque" />
+        <Items text="Code template for practice" />{" "}
+        <Items text="Access to interactive community" />
+        <Items text="All the Python code will be provided with the assignment, and there will be hands-on practice." />
+        <Items text="Connect and learn from like-minded peers who share your interests and goals." />
+        <Items text="Priority Support" />{" "}
+        <Items text="Discount/ Early access in the future" />
+        <Items text="We prioritize student queries and feedback to ensure they receive timely assistance." />
+        <Items text="You will receive big discounts on my upcoming courses!" />
       </div>
     </div>
   );
